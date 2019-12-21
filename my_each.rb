3acb39fa1c8d0e67864b0f95e2ec22a48032ed4c
Brigette LeVert <bnl.levert@gmail.com>
2019-12-21 22:28:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,18 @@
 def my_each # put argument(s) here
   # code here
   
+  if block_given?
+      i = 0 
+
+    while i < collection.length 
+     yield array[i]
+      i = i + 1 
+    end
+    collection
+  else
+    puts "Hey! No block was given!"
+  end
+end
   
   
   
